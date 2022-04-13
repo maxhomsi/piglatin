eb6e0e6174699c7cc802e8ce0ff1255b92111301
Ximilis <92741358+maxhomsi@users.noreply.github.com>
2022-04-13 16:19:28 -0500

    enter working

--- main.js
+++ main.js
@@ -1,17 +1,123 @@
 let button = document.getElementById('botao') //pegou o id do botao
 let input = document.getElementById('texto') //pegou id da caixa de texto
 
-button.addEventListener('click',  function(){ //pouco confuso aqui tb
-   let result = document.getElementById('resultado') //perguntar como enviar o formulario apertando enter
+input.addEventListener('keyup', function(e){
+  if (e.keyCode == 13){
+    
+    let result = document.getElementById('resultado') //perguntar como simplficar a funcao
    let text = input.value;
    input.value = '';
-
-
 
    //https://www.codegrepper.com/code-examples/javascript/how+to+play+audio+onclick+in+html
    //tutorial how to put sound on click.
    //check with teacher the command
-   
+
+   let sound = new Audio("pig.mp3"); 
+   input.addEventListener('keyup',()=>{
+       sound.currentTime = 0.3;
+       sound.play();
+   });
+   //codigo do projeto inicial.
+   const pigLatin = (word) => {
+    let acertar = word.toLowerCase().trim();
+    let position = positionFirstVowel(acertar);
+    let newWord = acertar;
+  
+    if (position == 0) {
+      newWord = acertar + "yay";
+      return newWord;
+    } else if (position > 0) {
+      let part1 = acertar.substring(0, position);
+      let part2 = acertar.substring(position);
+      //substring se usa para separar a palavra, tem q colocar as 2 partes, onde comeca e termina
+      //ex: "Hello"  text.substring(1, 4) ; resultado "ell"
+      newWord = part2 + part1 + "ay";
+      return newWord;
+    } else if ((position == -1)) {
+      newWord = acertar + "ay";
+      return newWord;
+    }
+  
+    //se a primeira letra for vogal, colocar "yay" no final da palavra. ex: eletanteyay
+    //se comecar comecar com consoante, pega a primeira letra, coloca no final e coloca AY, EX: BARCO = ARCOBAY
+    //se nao tiver vogal nenhuma coloca AY no final ex  PST, PSTAY
+    //pegar as consoantes iniciais,mover para o final e adicionar AY, EX: STRING = INGSTRAY
+  };
+  
+  // the first function called in the program to get an input from the user
+  // to run the function use the command: node main.js
+  // to close it ctrl + C
+  const getPrompt = () => {
+    rl.question('word ', (answer) => {
+      console.log( pigLatin(answer) );
+      getPrompt();
+    });
+  }
+ 
+  let positionFirstVowel = (acertar) => {
+    let aIdx = acertar.indexOf("a");
+    let eIdx = acertar.indexOf("e");
+    let iIdx = acertar.indexOf("i");
+    let oIdx = acertar.indexOf("o");
+    let uIdx = acertar.indexOf("u");
+    let len = acertar.length;
+  
+    let answer = -1;
+  
+    if (aIdx == -1) {
+    } else if (aIdx != -1 && answer == -1) {
+      answer = aIdx;
+    }
+    if (eIdx == -1) {
+    } else if (eIdx != -1 && answer == -1) {
+      answer = eIdx;
+    } else if (eIdx != -1 && answer >= eIdx) {
+      answer = eIdx;
+    }
+  
+    if (iIdx == -1) {
+    } else if (iIdx != -1 && answer == -1) {
+      answer = iIdx;
+    } else if (iIdx != -1 && answer >= iIdx) {
+      answer = iIdx;
+    }
+  
+    if (oIdx == -1) {
+    } else if (oIdx !== -1 && answer == -1) {
+      answer = oIdx;
+    } else if (oIdx !== -1 && answer >= oIdx) {
+      answer = oIdx;
+    }
+  
+    if (uIdx == -1) {
+    } else if (uIdx !== -1 && answer == -1) {
+      answer = uIdx;
+    } else if (uIdx !== -1 && answer >= uIdx) {
+      answer = uIdx;
+    }
+    return answer;
+  };
+  //duvida com relacao a isso. depois perguntar pro robert
+  let convertida = pigLatin(text)
+  resultado.innerText = convertida
+
+
+
+    console.log("apertou")  //tenho que colocar aqui o funcao que executa o codigo executar
+  }
+
+})
+
+button.addEventListener('click',  function(){ //pouco confuso aqui tb
+   let result = document.getElementById('resultado') //perguntar como enviar o formulario apertando enter 
+   //how to submit pressing enter
+   let text = input.value;
+   input.value = '';
+
+   //https://www.codegrepper.com/code-examples/javascript/how+to+play+audio+onclick+in+html
+   //tutorial how to put sound on click.
+   //check with teacher the command
+
    let sound = new Audio("pig.mp3"); 
    button.addEventListener('click',()=>{
        sound.currentTime = 0.3;
@@ -53,7 +159,7 @@ button.addEventListener('click',  function(){ //pouco confuso aqui tb
       getPrompt();
     });
   }
-  //coloquei aqui 
+ 
   let positionFirstVowel = (acertar) => {
     let aIdx = acertar.indexOf("a");
     let eIdx = acertar.indexOf("e");
